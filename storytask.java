85a9a4b663d18171618c10abfcb4528ed3552fe7
pavithra0911 <pavithrasrini16@gmail.com>
2024-11-21 13:14:40 +0530

    Add files via upload
    day2

---
+++ storytask.java
@@ -0,0 +1,30 @@
+public class storytask
+{
+public static void main(String args[])
+{
+String name1 ="Pavithra";
+String adjctive ="happy";
+String adjective ="joyfull";
+String noun1 ="animals";
+String noun2 ="chocolate";
+String verb1 ="clap";
+String noun3 ="guitar";
+String noun4 ="people";
+String adjective3 ="wonderfull";
+String name2 ="haripriya";
+String place1 ="thanjavur";
+String noun5 ="juice";
+int number ="2025";
+String noun6 ="plants";
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 
++ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to 
+" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ". 
+Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " + 
+name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world 
+where " + noun6 + "s ruled the world.";
+{
+System.out.println(storytask);
+}
+}
+}
