abc14f3244a509144b242533744bb2ccc264aacc
pavithra0911 <pavithrasrini16@gmail.com>
2024-11-26 13:09:29 +0530

    day5

---
+++ mark.java
@@ -0,0 +1,41 @@
+package day5;
+import java.util.Scanner;
+public class mark {
+	public static void main(String args[]) {
+//
+	Scanner sc=new Scanner(System.in);
+//			{
+//		System.out.println("enter the mark");
+//		int mark=sc.nextInt();
+//		
+//		if(mark>=40) {
+//			System.out.println("grade c");
+//		}
+//			else if(mark>=75) {
+//				System.out,println("grade B");
+//			}
+//				else if(mark>=90) {
+//					System.out println("grade A");
+//				}
+//				else {
+//					System.out println("fail");
+//				}
+//			}
+//		
+//
+//}
+//}
+		
+		
+		int num=0;
+		for(num=0;num<=10;num++)
+			System.out.println(num); 
+	
+System.out.println();
+
+System.out.println("enter the table");
+// 5*1=5
+int table=sc.nextInt();
+for(int i=1;i<=12;i++)
+	System.out.println(table+"x"+i+"="+i*table);
+}}
