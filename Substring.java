cb26168857ae1c4fca2af5411684df1ebf4e82dd
pavithra0911 <pavithrasrini16@gmail.com>
2024-11-28 12:52:57 +0530

    part-2

---
+++ Substring.java
@@ -0,0 +1,17 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Substring {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("enter the name");
+		String name=sc.nextLine();
+		System.out.println(name);
+		
+		
+	}
+
+}
