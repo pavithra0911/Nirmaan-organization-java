ad1299e50d93a5fb11621e57dd8dfff2ef2d37dd
pavithra0911 <pavithrasrini16@gmail.com>
2024-11-28 13:30:59 +0530

    part-3

---
+++ Student.java
@@ -0,0 +1,49 @@
+package day7;
+
+public class Student {
+	String name=" ";
+	int age;
+	String dateofBirth;
+	int rollNo;
+	
+	
+	public static void main(String args[]) {
+		Student student1= new Student();
+		student1.name="pavithra";
+		student1.age=21;
+		student1.dateofBirth="9/11/2003";
+		student1.rollNo=2103546;
+		System.out.println(student1.name);
+		System.out.println(student1.age);
+		System.out.println(student1.dateofBirth);
+		System.out.println(student1.rollNo);
+				
+		
+
+		Student student2= new Student();
+		student2.name="hari priya";
+		student2.age=21;
+		student2.dateofBirth="28/07/2003";
+		student2.rollNo=2103547;
+		System.out.println(student2.name);
+		System.out.println(student2.age);
+		System.out.println(student2.dateofBirth);
+		System.out.println(student2.rollNo);
+				
+				
+				
+		Student student3= new Student();
+		student3.name="sankari";
+		student3.age=21;
+		student3.dateofBirth="18/06/2004";
+		student3.rollNo=2103548;
+		System.out.println(student3.name);
+		System.out.println(student3.age);
+		System.out.println(student3.dateofBirth);
+		System.out.println(student3.rollNo);
+				
+		
+	}
+	
+
+}
