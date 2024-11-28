c4fff59cb2489eae03eff5c44ba0a981f590fe1f
pavithra0911 <pavithrasrini16@gmail.com>
2024-11-28 12:50:08 +0530

    day 7 part-1

---
+++ Nestedforloop.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class Nestedforloop {
+	public static void main(String args[])
+	{
+		for(int i=1;i<=20;i++) {
+			for(int j=1;j<=4;j++) {
+				System.out.println(j);
+				
+				}
+			}
+		}
+	}
+
+
