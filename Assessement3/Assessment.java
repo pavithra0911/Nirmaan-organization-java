63f8edf87b05b971dc4c7e5cc3ebcf868d8f5625
pavithra0911 <pavithrasrini16@gmail.com>
2024-12-30 11:29:07 +0530

    assessement

---
+++ Assessement3/Assessment.java
@@ -0,0 +1,74 @@
+package Assessement3;
+
+public class Assessment {
+	public static void main(String args[]) {
+//	1.count the odd number
+//		int count=0;
+//		for(int i=1;i<=100;i++) {
+//			if(i%2!=0) {
+//				System.out.println(i);
+//				count++;
+//			}
+//		}
+//	System.out.println("count odd number:"+count);	
+//	}
+//
+
+//2.count the even number
+//  int count=0;
+//    	for(int i=1;i<=100;i++) {
+//    		if(i%2==0) {
+//    			System.out.println(i);
+//    			count++;
+//    			
+//    			
+//    		}
+//    	}
+//    System.out.println("cont even number:"+count);
+	
+    	
+//    3.sum of the even number
+//		int sum=0;
+//		for(int i=1;i<=100;i++) {
+//			if(i%2==0) {
+//				sum+=i;
+//				
+//			}
+//		} 
+//		System.out.println("total the even number:"+sum);
+//	}}
+//		4.sum of the odd number
+//		
+//		int sum=0;
+//		for(int i=1;i<=100;i++) {
+//			if(i%2!=0) {
+//				sum+=i;
+//				
+//			}
+//		}
+//		System.out.println("total the odd number:"+sum);
+//	}}
+		
+		
+//	5.reverse string
+//String name="pavithra";
+//for(int i=7;i>=0;i--) {
+//	System.out.print(name.charAt(i));
+//	
+//}
+//
+//
+//	
+//	}}	
+		
+//		6.factorial
+		
+	int i,fact=1;
+	int a=5;
+	for(i=1;i<=a;i++) {
+		fact=fact*i;
+	}
+	System.out.println("factorial of 5 is:"+fact);
+	}
+		
+	}
